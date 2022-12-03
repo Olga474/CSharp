c8dd777825b71a7f35bef156d49685fc0121ab40
olga <9672503863olya@mail.ru>
2022-12-03 03:29:16 +0300

    add ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число a:");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число b:");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число c:");
+int c = int.Parse(Console.ReadLine()!);
+
+if ((a>=b)&(a>=c)) {
+    Console.WriteLine($"max = {a}");
+} 
+else if ((b>=a)&(b>=c)) {
+    Console.WriteLine($"max = {b}");
+} 
+else {
+    Console.WriteLine($"max = {c}");
+}
