841d8e208732d4c38e4216326649933f00d515eb
olga <9672503863olya@mail.ru>
2022-12-06 21:24:44 +0300

    add ex12

---
+++ ex12/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if ((n > 0) & (n < 6)) {
+    Console.WriteLine("нет");
+} else if ((n > 5) & (n < 8)) {
+    Console.WriteLine("да");
+} else {Console.WriteLine("Неверные данные");}
