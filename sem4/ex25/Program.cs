54153f88787933d8d40aaeedf1211a77d0676c18
FonarevaOlga <oefonareva@edu.hse.ru>
2022-12-16 17:39:18 +0300

    Add ex25

---
+++ sem4/ex25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int a1 = a;
+Console.Write("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < b; i++) {
+    a = a1*a;
+}
+
+Console.WriteLine($"{a1}^{b}={a}");
