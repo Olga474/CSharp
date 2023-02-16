dff77956214bf05d2ab6822805ef3f72a4a2bef7
olga <9672503863olya@mail.ru>
2023-02-16 23:48:21 +0300

    new

--- ex10/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.Write("Введите трехзначное число: ");
-int n = int.Parse(Console.ReadLine()!);
-n = n/10;
-n = n%10;
-
-Console.WriteLine($"{n}");
--- ex11/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Введите число: ");
-int n = int.Parse(Console.ReadLine()!);
-if (n<100) {
-    Console.WriteLine("Третьей цифры нет");
-} else {
-    string s = n.ToString();
-    
-    Console.WriteLine($"{s[2]}");
-}
